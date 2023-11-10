fa04c7209d29910236526fa08dff19637f8aa148
Rana Juniyat <rana412004je@gmail.com>
2023-11-10 18:09:44 +0200

    Menu Function

---
+++ main.py
@@ -0,0 +1,15 @@
+##########################
+#       Main & Menu
+##########################
+
+def menu():
+    print("\nWelcome to the Advanced Browser Tabs Simulation!")
+    print("1. Open Tab")
+    print("2. Close Tab")
+    print("3. Switch Tab")
+    print("4. Display All Tabs")
+    print("5. Open Nested Tab")
+    print("6. Clear All Tabs")
+    print("7. Save Tabs")
+    print("8. Import Tabs")
+    print("9. Exit")
