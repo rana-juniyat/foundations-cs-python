804166d0dddccd4cec6be8e2f6af8f9f5c2c8840
Rana Juniyat <rana412004je@gmail.com>
2023-11-10 18:29:13 +0200

    Implemented main menu and the structure of the main function.

--- main.py
+++ main.py
@@ -13,3 +13,33 @@ def menu():
     print("7. Save Tabs")
     print("8. Import Tabs")
     print("9. Exit")
+
+def main():
+    while True:
+        menu()
+        choice = input("Enter your choice (1-9): ")
+
+        if choice == "1":
+            add_tab()
+        elif choice == "2":
+            close_tab()
+        elif choice == "3":
+            switch_tab()
+        elif choice == "4":
+            display_all_tabs()
+        elif choice == "5":
+            open_nested_tab()
+        elif choice == "6":
+            clear_all_tabs()
+        elif choice == "7":
+            save_tabs()
+        elif choice == "8":
+            import_tabs()
+        elif choice == "9":
+            print("Exiting the program. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please enter a number between 1 and 9.")
+
+if __name__ == "__main__":
+    main()
